78b5b6efba09d243f946f518d59dc382b1411411
AdithyaKahawanugoda <adithyakahawanugoda@gmail.com>
2022-07-22 23:27:24 +0530

    frontend admin page layout configured

--- frontend/src/pages/admin/Admin.jsx
+++ frontend/src/pages/admin/Admin.jsx
@@ -1,8 +1,24 @@
 import React from "react";
 import "./Admin.css";
+import { ContainerCard } from "../../components/index";
 
 const Admin = () => {
-  return <div>Admin</div>;
+  return (
+    <>
+      <div class="admin-page-angry-grid">
+        <div id="admin-page-item-0">&nbsp;0</div>
+        <div id="admin-page-item-1">
+          <ContainerCard>User List section</ContainerCard>
+        </div>
+        <div id="admin-page-item-2">&nbsp;2</div>
+        <div id="admin-page-item-3">&nbsp;3</div>
+        <div id="admin-page-item-4">
+          <ContainerCard>Pagination section</ContainerCard>
+        </div>
+        <div id="admin-page-item-5">&nbsp;5</div>
+      </div>
+    </>
+  );
 };
 
 export default Admin;
--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -5,14 +5,14 @@ import "./Home.css";
 const Home = () => {
   return (
     <>
-      <div className="angry-grid">
-        <div id="item-0">&nbsp;0</div>
-        <div id="item-1">
+      <div className="home-page-angry-grid">
+        <div id="home-page-item-0">&nbsp;0</div>
+        <div id="home-page-item-1">
           <ContainerCard>Hi</ContainerCard>
         </div>
-        <div id="item-2">&nbsp;2</div>
-        <div id="item-3">&nbsp;3</div>
-        <div id="item-4">&nbsp;4</div>
+        <div id="home-page-item-2">&nbsp;2</div>
+        <div id="home-page-item-3">&nbsp;3</div>
+        <div id="home-page-item-4">&nbsp;4</div>
       </div>
     </>
   );
