df0a8cade38db9ad3cf3eb690ea4c2313643321d
AdithyaKahawanugoda <adithyakahawanugoda@gmail.com>
2022-07-22 22:50:57 +0530

    frontend page routes added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,18 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter as BRouter, Routes, Route } from "react-router-dom";
+import { Home, Admin, Student } from "./pages/index";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <BRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/student" element={<Student />} />
+      </Routes>
+    </BRouter>
+  );
 };
 
 export default App;
