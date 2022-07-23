5cf3fec4189919da92dabd41c0637367bdf1e6af
AdithyaKahawanugoda <adithyakahawanugoda@gmail.com>
2022-07-24 01:59:48 +0530

    backend get all users - pagination logic added

--- backend/controllers/user-controller.js
+++ backend/controllers/user-controller.js
@@ -37,9 +37,30 @@ exports.updateProfile = async (req, res) => {
 // get all users
 exports.getUsers = async (req, res) => {
   try {
-    const allUsers = await UserModel.find();
-    return res.status(200).send({
-      allUsers,
+    let query = UserModel.find();
+
+    const page = parseInt(req.query.page) || 1;
+    const pageSize = parseInt(req.query.limit) || 4;
+    const skip = (page - 1) * pageSize;
+    const total = await UserModel.countDocuments();
+
+    const pages = Math.ceil(total / pageSize);
+
+    query = query.skip(skip).limit(pageSize);
+
+    if (page > pages) {
+      return res.status(404).json({
+        msg: "No page found",
+      });
+    }
+
+    const users = await query;
+
+    res.status(200).json({
+      count: users.length,
+      page,
+      pages,
+      users,
     });
   } catch (error) {
     return res.status(500).json({
