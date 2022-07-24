147b5b1c0127b2b7bc00b424494ffc8fcc8598c7
AdithyaKahawanugoda <adithyakahawanugoda@gmail.com>
2022-07-24 19:07:00 +0530

    FE note management implemented and BE integrated

--- frontend/src/components/notes/Notes.jsx
+++ frontend/src/components/notes/Notes.jsx
@@ -1,8 +1,148 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Notes.css";
+import { Pagination, AddNewNote } from "../index";
+import ModeEditOutlineRoundedIcon from "@mui/icons-material/ModeEditOutlineRounded";
+import DeleteForeverRoundedIcon from "@mui/icons-material/DeleteForeverRounded";
+import Modal from "@mui/material/Modal";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import { teal, red } from "@mui/material/colors";
+import axios from "axios";
 
 const Notes = () => {
-  return <div>Notes</div>;
+  const [notesList, setNotesList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [page, setPage] = useState(1);
+  const [pages, setPages] = useState(1);
+  const [selectedNote, setSelectedNote] = useState(null);
+  const [openUpdate, setOpenUpdate] = useState(false);
+  const [openDelete, setOpenDelete] = useState(false);
+
+  useEffect(() => {
+    console.log(1);
+    const getNotes = async () => {
+      console.log(2);
+      setIsLoading(true);
+      const config = {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("authToken")}`,
+        },
+      };
+      await axios
+        .get(`http://localhost:5000/api/note/getAll?page=${page}`, config)
+        .then((res) => {
+          console.log(res);
+          setPages(res.data.pages);
+          setNotesList(res.data.notes);
+          setIsLoading(false);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    };
+    if (
+      localStorage.getItem("authToken") &&
+      localStorage.getItem("accountType") === "STUDENT"
+    ) {
+      getNotes();
+    }
+  }, [page]);
+
+  const modalStyles = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 400,
+    bgcolor: "background.paper",
+    border: "2px solid #000",
+    boxShadow: 24,
+    p: 4,
+  };
+
+  return (
+    <div className="note-list-container">
+      <div className="note-list-container-heading">User list</div>
+      <div className="note-list-item-container">
+        <div className="note-list-item-headings">
+          <div className="note-list-item-column1">Title</div>
+          <div className="note-list-item-column2">Description</div>
+          <div className="note-list-item-column3">Update</div>
+          <div className="note-list-item-column4">Delete</div>
+        </div>
+        <div className="note-list-item-container-boundary">
+          {notesList?.length > 0 &&
+            notesList.map((note, index) => {
+              return (
+                <div className="note-list-item-boundary" key={index}>
+                  <div className="note-list-item-column1">{note?.title}</div>
+                  <div className="note-list-item-column2">
+                    {note?.description}
+                  </div>
+                  <div className="note-list-item-column3">
+                    <ModeEditOutlineRoundedIcon
+                      onClick={() => {
+                        setSelectedNote(note);
+                        setOpenUpdate(true);
+                      }}
+                      style={{ color: teal[500] }}
+                    />
+                  </div>
+                  <div className="note-list-item-column4">
+                    <DeleteForeverRoundedIcon
+                      onClick={() => {
+                        setSelectedNote(note);
+                        setOpenDelete(true);
+                      }}
+                      style={{ color: red[500] }}
+                    />
+                  </div>
+                </div>
+              );
+            })}
+        </div>
+
+        <div className="note-list-pagination">
+          <Pagination page={page} pages={pages} changePage={setPage} />
+        </div>
+        {!isLoading && selectedNote && (
+          <Modal
+            open={openUpdate}
+            onClose={() => {
+              setOpenUpdate(false);
+              setSelectedNote(null);
+            }}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+          >
+            <Box sx={modalStyles}>
+              <Typography id="modal-modal-title" variant="h6" component="h2">
+                Note Update Window
+              </Typography>
+              <AddNewNote selectedNote={selectedNote} />
+            </Box>
+          </Modal>
+        )}
+        {!isLoading && selectedNote && (
+          <Modal
+            open={openDelete}
+            onClose={() => {
+              setOpenDelete(false);
+              setSelectedNote(null);
+            }}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+          >
+            <Box sx={modalStyles}>
+              <Typography id="modal-modal-title" variant="h6" component="h2">
+                Delete Confirmation Window
+              </Typography>
+            </Box>
+          </Modal>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Notes;
--- frontend/src/pages/student/Student.jsx
+++ frontend/src/pages/student/Student.jsx
@@ -1,23 +1,41 @@
 import React from "react";
 import "./Student.css";
-import { ContainerCard } from "../../components/index";
+import { ContainerCard, Notes, AddNewNote } from "../../components/index";
+import { useNavigate } from "react-router-dom";
 
 const Student = () => {
+  const navigate = useNavigate();
+
+  const logoutHandler = () => {
+    localStorage.removeItem("authToken");
+    localStorage.removeItem("accountType");
+    navigate("/");
+  };
+
   return (
     <>
-      <div class="student-page-angry-grid">
+      <div className="student-page-angry-grid">
         <div id="student-page-item-0">
-          <ContainerCard>Add new note section</ContainerCard>
+          <ContainerCard>
+            <AddNewNote />
+          </ContainerCard>
         </div>
         <div id="student-page-item-1">
-          <ContainerCard>Notes list section</ContainerCard>
+          <ContainerCard>
+            <Notes />
+          </ContainerCard>
         </div>
-        <div id="student-page-item-2">&nbsp;2</div>
-        <div id="student-page-item-3">&nbsp;3</div>
-        <div id="student-page-item-4">&nbsp;4</div>
-        <div id="student-page-item-5">&nbsp;5</div>
-        <div id="student-page-item-6">&nbsp;6</div>
-        <div id="student-page-item-7">&nbsp;7</div>
+        <div id="student-page-item-2">
+          <div className="student-page-header">STUDENT VIEW</div>
+          <div className="student-page-header-options" onClick={logoutHandler}>
+            LOG OUT
+          </div>
+        </div>
+        <div id="student-page-item-3"></div>
+        <div id="student-page-item-4"></div>
+        <div id="student-page-item-5"></div>
+        <div id="student-page-item-6"></div>
+        <div id="student-page-item-7"></div>
       </div>
     </>
   );
