aaf8accb176f0c6a99e45732dd13b8bd426fdbd4
AdithyaKahawanugoda <adithyakahawanugoda@gmail.com>
2022-07-24 20:24:37 +0530

    note delete FE and BE integrated

--- frontend/src/components/notes/Notes.jsx
+++ frontend/src/components/notes/Notes.jsx
@@ -3,6 +3,9 @@ import "./Notes.css";
 import { Pagination, AddNewNote } from "../index";
 import ModeEditOutlineRoundedIcon from "@mui/icons-material/ModeEditOutlineRounded";
 import DeleteForeverRoundedIcon from "@mui/icons-material/DeleteForeverRounded";
+import CheckCircleRoundedIcon from "@mui/icons-material/CheckCircleRounded";
+import DoNotDisturbAltRoundedIcon from "@mui/icons-material/DoNotDisturbAltRounded";
+import Button from "@mui/material/Button";
 import Modal from "@mui/material/Modal";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
@@ -19,9 +22,7 @@ const Notes = () => {
   const [openDelete, setOpenDelete] = useState(false);
 
   useEffect(() => {
-    console.log(1);
     const getNotes = async () => {
-      console.log(2);
       setIsLoading(true);
       const config = {
         headers: {
@@ -47,6 +48,26 @@ const Notes = () => {
       getNotes();
     }
   }, [page]);
+
+  const deleteNoteHandler = async (selectedNote) => {
+    const config = {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("authToken")}`,
+      },
+    };
+    await axios
+      .delete(
+        `http://localhost:5000/api/note//deleteById/${selectedNote._id}`,
+        config
+      )
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    setSelectedNote(null);
+  };
 
   const modalStyles = {
     position: "absolute",
@@ -134,9 +155,39 @@ const Notes = () => {
             aria-describedby="modal-modal-description"
           >
             <Box sx={modalStyles}>
-              <Typography id="modal-modal-title" variant="h6" component="h2">
-                Delete Confirmation Window
+              <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                Are you sure you want to permanently delete this note?
               </Typography>
+              <div className="note-delete-confirmation-options">
+                <Button
+                  variant="contained"
+                  size="medium"
+                  style={{ backgroundColor: teal[500] }}
+                  endIcon={<DoNotDisturbAltRoundedIcon />}
+                  onClick={() => {
+                    setOpenDelete(false);
+                    setSelectedNote(null);
+                  }}
+                >
+                  <div className="note-delete-confirmation-options-text">
+                    CANCEL
+                  </div>
+                </Button>
+                <Button
+                  variant="contained"
+                  size="medium"
+                  style={{ backgroundColor: red[500] }}
+                  endIcon={<CheckCircleRoundedIcon />}
+                  onClick={() => {
+                    deleteNoteHandler(selectedNote);
+                    setOpenDelete(false);
+                  }}
+                >
+                  <div className="note-delete-confirmation-options-text">
+                    DELETE
+                  </div>
+                </Button>
+              </div>
             </Box>
           </Modal>
         )}
