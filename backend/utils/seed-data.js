a077a19576fba472389ebca858ec564291f00503
AdithyaKahawanugoda <adithyakahawanugoda@gmail.com>
2022-07-25 19:35:06 +0530

    admin data seeding script added

---
+++ backend/utils/seed-data.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+const UserModel = require("../models/user-model");
+const validator = require("validator");
+const fs = require("fs");
+const path = require("path");
+require("dotenv").config();
+
+const parsedData = JSON.parse(
+  fs.readFileSync(path.join(__dirname, "admin-data.json"), "utf-8")
+);
+
+// admin account add
+const addAdmin = async (data) => {
+  if (
+    data.email &&
+    data.password &&
+    data.accountType === "ADMIN" &&
+    data.id === 0
+  ) {
+    try {
+      // validate email using official standard RFC 5322
+      if (!validator.isEmail(data.email)) {
+        console.log("Invalid email address, please check again");
+      }
+      await UserModel.create(data);
+      console.log("Admin account created");
+      console.log({ email: data.email, password: data.password });
+    } catch (error) {
+      console.log("Error occurred!", error);
+    }
+    process.exit();
+  }
+};
+
+// const URI = process.env.MONGO_URI;
+const URI = "mongodb://localhost:27017";
+
+// mongodb connection
+mongoose
+  .connect(URI, { dbName: "surge_db" })
+  .then(() => {
+    console.log("MongoDB Connection Success");
+    addAdmin(parsedData);
+  })
+  .catch((err) => {
+    console.log("Connection Failed - " + err);
+  });
